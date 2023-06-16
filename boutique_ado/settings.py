bf2cad1710ca32e9da6f63cf5394e62b7bc8dcd8
AdamRalph123 <adambyrne16@gmail.com>
2023-06-16 18:39:12 +0000

    Added stripe elements

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-adamralph12-boutiqueald-lpmw7lg4g3z.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['8000-adamralph12-boutiqueald-lpmw7lg4g3z.ws-eu100.gitpod.io']
 
 
 # Application definition
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NJgzpGnSdbDBu2BufhDG1TOek7B2RKoeAqkAMxAB6zhAb6ojJ2Nq51JN5PGhjXKtCYSCyt1qbtZO5VEIZPaeyPh00FfO5jGFg',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
